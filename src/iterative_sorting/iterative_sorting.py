f3c66e05d24aa9a6049944df5fcb1e180a096b9d
Sophia Sagan <sophiabraddock82@gmail>
2020-07-08 21:00:56 -0400

    mvp working on stretch

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,4 +1,18 @@
 # TO-DO: Complete the selection_sort() function below
+'''
+repeat (numOfElements - 1) times
+
+  set the first unsorted element as the minimum
+
+  for each of the unsorted elements
+
+    if element < currentMinimum
+
+      set element as new minimum
+
+  swap minimum with first unsorted position
+  '''
+
 def selection_sort(arr):
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
@@ -6,18 +20,40 @@ def selection_sort(arr):
         smallest_index = cur_index
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc)
-        # Your code here
+        for j in range(i + 1, len(arr)):
+            if arr[j] < arr[smallest_index]:
+                smallest_index = j
 
 
         # TO-DO: swap
-        # Your code here
+        arr[i], arr[smallest_index] = arr[smallest_index], arr[i]
 
     return arr
 
 
 # TO-DO:  implement the Bubble Sort function below
+'''do
+
+  swapped = false
+
+  for i = 1 to indexOfLastUnsortedElement-1
+
+    if leftElement > rightElement
+
+      swap(leftElement, rightElement)
+
+      swapped = true; swapCounter++
+
+while swapped'''
+
 def bubble_sort(arr):
-    # Your code here
+    swap_made = True
+    while swap_made == True:
+        swap_made = False
+        for i in range(0, len(arr) - 1):
+            if arr[i] > arr[i+1]:
+                arr[i], arr[i+1] = arr[i+1], arr[i]
+                swap_made = True
 
 
     return arr
@@ -39,8 +75,43 @@ buckets.
 
 What is the time and space complexity of the counting sort algorithm?
 '''
-def counting_sort(arr, maximum=None):
-    # Your code here
+# O(n+k) : Time
+# O(n + k) : Space
 
+'''
+create key (counting) array
+
+for each element in list
+
+  increase the respective counter by 1
+
+for each counter, starting from smallest key
+
+  while counter is non-zero
+
+    restore element to list
+
+    decrease counter by 1
+    '''
+
+def counting_sort(arr, maximum=None):
+    count_arr = [0 for i in range(maximum)]
+    count = [0 for i in range(maximum)]
+
+    ans = ["" for _ in arr]
+
+    for i in arr:
+        count[ord(i)] += 1
+
+    for i in range(maximum):
+        count[i] += count[i-1]
+
+    for i in range(len(arr)):
+        count_arr[count[ord(arr[i])] -1] = arr[i]
+        count[ord(arr[i])] -= 1
+
+    for i in range(len(arr)):
+        ans[i] = count_arr[i]
+        count_arr[i] = arr[i]
 
     return arr
